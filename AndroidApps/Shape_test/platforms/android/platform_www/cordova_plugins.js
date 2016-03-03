b5ef78fb8ed327912ffd1f55e9bdba46569c9243
Sonal <sonals9227@gmail.com>
2016-03-03 16:14:53 +0530

    Sample android app Shape_test added

---
+++ AndroidApps/Shape_test/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1"
+}
+// BOTTOM OF METADATA
+});
